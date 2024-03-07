bddde98fabfa44bc3f9963a54f5a896dce379096
Manuel Meneses <hbiaser132@gmail.com>
2024-03-07 08:39:44 -0400

    make build

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('test')
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
   }
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
-
+import { readFile } from 'fs/promises';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello() {
+    const file = await readFile('package.json', 'utf-8');
+    return file;
   }
 }
