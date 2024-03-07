d89bb78ab5a10fca18b3c9155794bc8067b8aa97
Manuel Meneses <hbiaser132@gmail.com>
2024-03-07 07:43:23 -0400

    make build

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  await app.listen(5019);
+  await app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+  });
 }
 bootstrap();
