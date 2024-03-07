c959aad251541cfe252909d501b1a00572c9b64e
Manuel Meneses <hbiaser132@gmail.com>
2024-03-07 07:41:48 -0400

    make build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    UsersModule,
-    AuthModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://leduc1901:melody19@kylecluster.abjzu.mongodb.net/?retryWrites=true&w=majority',
-    ),
+    // MongooseModule.forRoot(process.env.MONGO_DB),
     ConfigModule.forRoot(),
   ],
   controllers: [AppController],
