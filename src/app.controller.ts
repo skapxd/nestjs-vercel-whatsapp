85bc491e500c3a7e08d502e4f95e4c02f526b7a8
Manuel Meneses <hbiaser132@gmail.com>
2024-03-08 19:59:32 -0400

    abc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { SendMessageDTO } from './dto/send-message.dto';
+import { AuthorizationGuard } from './authorization/authorization.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post()
+  @UseGuards(AuthorizationGuard)
   sendMessage(@Body() dto: SendMessageDTO) {
     return this.appService.sendMessage(dto);
   }
