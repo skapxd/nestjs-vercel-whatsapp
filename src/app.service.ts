bbc93cc480b662e7dcf71d1b2c1cdf6a0d4da410
Manuel Meneses <hbiaser132@gmail.com>
2024-03-09 08:05:29 -0400

    abc

--- src/app.service.ts
+++ src/app.service.ts
@@ -115,7 +115,7 @@ export class AppService {
 
   async sendMessage(dto: SendMessageDTO) {
     try {
-      this.sock.sendMessage(`${dto.phone}@s.whatsapp.net`, {
+      await this.sock.sendMessage(`${dto.phone}@s.whatsapp.net`, {
         text: dto.message,
       });
     } catch (error) {
